583eed734f2f1b87188e3639f9ad7dfb687a4a39
zenpo <zenpo.cheong2022@gmail.com>
2025-10-01 20:43:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zenpo",
-    version="1.2.6",
+    version="2.0.0",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
